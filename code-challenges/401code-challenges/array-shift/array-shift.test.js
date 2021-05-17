8878e575af06e5140a5f81111e1efbf9213857fa
NeveenBeiram <neveenbeiram@yahoo.com>
2021-05-17 22:59:25 +0300

    done

---
+++ code-challenges/401code-challenges/array-shift/array-shift.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let insertShiftArray=require('./array-shift.js');
+
+
+
+describe('testing the insert shift array function',()=>{
+  it('it should return array with the value in the middle of it',()=>{
+    expect(insertShiftArray([30,8,17,55], 12)).toEqual([30,8,12,17,55]);
+    expect(insertShiftArray([2,7,20,23,30], 21)).toEqual([2,7,20,21,23,30]);
+    expect(insertShiftArray([], 10)).toEqual([10]);
+  });
+});
+
