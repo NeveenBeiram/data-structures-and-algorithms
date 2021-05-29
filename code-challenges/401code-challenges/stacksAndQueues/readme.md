3f492eb363c8372a4c2396876c687ed33a28420a
NeveenBeiram <neveenbeiram@yahoo.com>
2021-05-29 21:32:20 +0300

    ..

---
+++ code-challenges/401code-challenges/stacksAndQueues/readme.md
@@ -0,0 +1,31 @@
+# Stacks and Queues
+
+The stack and the queue are a data structure that consists of Nodes. Each Node references the next Node in the stack/in the queue.
+
+- LIFO: Last In First Out(stack)
+
+- FIFO: First In First Out(queue)
+
+## Challenge
+
+Stack class with these methods: push,pop,peek,isEmpty  
+
+Queue class with these methods: enqueue,dequeue,peek,isEmpty
+
+## Approach & Efficiency
+
+* Stack: 
+    - push Time: O(1) Space:O(1)
+    - pop Time: O(1) Space:O(1)
+    - peek Time: O(1) Space:O(1)
+    - isEmpty Time: O(1) Space:O(0)
+
+* queue: 
+    - enqueue Time: O(1) Space:O(1)
+    - dequeue Time: O(1) Space:O(1)
+    - peek Time: O(1) Space:O(1)
+    - isEmpty Time: O(1) Space:O(0)
+
+
+## API
+![img]()
---
+++ code-challenges/401code-challenges/stacksAndQueues/stacks-and-queues.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const Node = require('./node');
+
+class Stack{
+  constructor(){
+    this.top=null;
+  }
+
+  push(value){
+    let node=new Node(value);
+    try{
+      if(this.isEmpty()){
+        this.top=node;
+      }else{
+        node.next=this.top;
+        this.top=node;
+      }
+    }catch(err){
+      throw new Error('you must enter a value to add it',err);
+    }
+  }
+
+  pop(){
+    try{
+      if(this.top){
+        let val=this.top.value;
+        this.top=this.top.next;
+        return val;
+      }
+
+    }catch(err){
+      throw new Error(`it is an empty stack you can't pop from it `,err);
+    }
+
+  }
+
+  peek(){
+    if(this.top){
+      return this.top.value;
+    }else{
+      throw new Error(`it's an empty stack`);
+    }
+
+  }
+  isEmpty(){
+    if(this.top)
+      return false;
+    return true;
+  }
+
+}
+
+class Queue{
+  constructor(){
+    this.front =null;
+    this.tail=null;
+  }
+
+  enqueue (value){
+    let node=new Node(value);
+    try{
+      while(this.isEmpty){
+        this.front=node;
+        this.tail=node;
+      }
+      this.tail.next=node;
+      this.tail=node;
+    }catch(err){
+      throw new Error(`you must enter a value to add it`,err);
+    }
+  }
+
+  dequeue(){
+    try{
+      while(!this.isEmpty){
+        let val=this.front.value;
+        this.front=this.front.next;
+        return val;
+      }
+    }catch(err){
+      throw new Error(`it's an empty queue can't dequeue from it`);
+    }
+  }
+  peek(){
+    try{
+      if(!this.isEmpty){
+        let val= this.front.value;
+        return val;
+      }
+    }catch(err){
+      throw new Error(`it's an empty queue!`);
+    }
+  }
+
+  isEmpty(){
+    if(this.front)return false;
+    return true;
+  }
+
+}
+
+
+
+module.exports={
+  Stack :Stack,
+  Queue :Queue
+};
+// module.exports={Stack,Queue};
