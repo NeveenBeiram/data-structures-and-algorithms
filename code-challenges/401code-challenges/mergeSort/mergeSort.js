2bb60acd6c4906325a87d173919397433813a3e6
NeveenBeiram <neveenbeiram@yahoo.com>
2021-06-21 17:43:51 +0300

    done

---
+++ code-challenges/401code-challenges/mergeSort/mergeSort.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+function merge (left, right, arr){
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < left.length && j < right.length){
+    if (left[i] <= right[j]){
+      arr[k] = left[i];
+      i = i + 1;
+    }else {
+      arr[k] = right[j];
+      j = j + 1;
+    }
+    k = k + 1;
+  }
+
+  while (j < right.length) {
+    arr[k] = right[j];
+    j= j+1;
+    k= k+1;
+  }
+
+  while (i < left.length) {
+    arr[k] = left[i];
+    i= i+1;
+    k= k+1;
+  }
+  return arr;
+}
+
+function mergeSort(arr){
+
+  try{
+
+    if(arr){
+      let n = arr.length;
+      if (n > 1){
+        let mid = n / 2;
+        let left = arr.slice(0, mid);
+        let right = arr.slice(mid, n);
+        mergeSort(left);
+        mergeSort(right);
+        return merge(left, right, arr);
+      }
+    }else return `you should enter an array`;
+  }catch(e){
+    return `you should enter an array`;
+  }
+}
+
+module.exports=mergeSort;
